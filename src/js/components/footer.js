92b70632e75e92d17f7f48d573096ea0e61d22f1
Nazareno Lorenzo <nazarenolorenzo@gmail.com>
2020-03-22 04:23:16 +0000

    :art: Change footer text

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -4,6 +4,13 @@ class Footer extends Component {
   render() {
     return (
       <div className="section end">
+        <p>
+          <strong>This is not a travel guide.</strong> This is a log of my
+          experiences traveling, and so, it shows what I, Naz, experienced and
+          felt in these places. You may disagree in many things, and that's ok.
+          You may enjoy similar things as me, and then this could help you plan
+          your trip.
+        </p>
         <h4 className="h2">The end</h4>
         <p>
           Travel Log by Nazareno Lorenzo, based on a Design by
