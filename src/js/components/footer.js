0139988b29e6ac7209c255a3d5d51d73f4976cc4
Nazareno Lorenzo <nazarenolorenzo@gmail.com>
2020-03-29 08:40:11 +0100

    :art: Minor changes in footer

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -5,15 +5,14 @@ class Footer extends Component {
     return (
       <div className="section end">
         <p>
-          <strong>This is not a travel guide.</strong> This is a log of my
-          experiences traveling, and so, it shows what I, Naz, experienced and
-          felt in these places. You may disagree in many things, and that's ok.
-          You may enjoy similar things as me, and then this could help you plan
-          your trip.
+          <strong className="h2">This is not a travel guide.</strong> This is a
+          log of my experiences traveling, and so, it shows what I, Naz,
+          experienced and felt in these places. You may disagree in many things,
+          and that's ok. You may enjoy similar things as me, and then this could
+          help you plan your trip.
         </p>
-        <h4 className="h2">The end</h4>
         <p>
-          Travel Log by Nazareno Lorenzo, based on a Design by
+          Made by myself, based on a Design by{" "}
           <a href="https://laurenhallden.com" title="Lauren Hallden, Designer">
             Lauren Hallden
           </a>
