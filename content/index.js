788bd4101e081c89721a33133817bed73bd46df8
Nazareno Lorenzo <nazarenolorenzo@gmail.com>
2020-04-06 07:04:53 +0100

    :wastebasket: Cleaned up content/index.js

--- content/index.js
+++ content/index.js
@@ -1,19 +1,3 @@
-import App from "./components/app";
-import '../sass/screen.scss';
-
 var chapters = {
-  // give each location the same name as its section id, and set its coordinates here
-  "location-one": {
-    center: [-75.40171, 41.221494],
-    zoom: 12.5,
-    bearing: 35,
-    pitch: 40,
-    duration: 8000
-  },
-  "location-two": {
-    center: [-75.748217, 40.740883],
-    zoom: 11,
-    bearing: 55,
-    pitch: 70
-  }
+  tokyo: require("tokyo.json")
 };
